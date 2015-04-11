3b8bfc0fc53c3ad56b5100a903524de29f647515
amera <amera.elsayed6@gmail.com>
2015-04-11 21:18:49 +0200

    final repo

---
+++ ERP/myaddons/iti/__openerp__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'ourWareHouse',
+    'version': '1.0',
+    'author': 'The Best Team Ever',
+    'category': 'Business Modules',
+    'summary': 'simple warehouse system',
+    'description':"""Simple Warehouse System""",
+    'website': 'http://www.openerp.com',
+    'depends': [],
+    'data': ['warehouse_view.xml',
+             'myhr_view.xml',
+             'report/stockZ_report_view.xml',
+             'stockZ_view.xml'
+             ],
+    'installable': True,
+    'auto_install': False,
+}
---
+++ ERP/myaddons/iti/report/session.mako
@@ -0,0 +1,10 @@
+<html>
+<head>
+<style type="text/css">${css}</style>
+</head>
+<body>
+<h1>${"Stock Report"}</h1>
+
+
+</body>
+</html>
